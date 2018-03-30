ee991f5ee13dcb410d55a1633ca39d762989ed60
Shubham Kumar <shubham.kumar.sci@gmail.com>
2018-03-31 02:18:27 +0530

    Reformatted Code, Added question.pdf

--- HTTP_Client/HTTP_Client.cpp
+++ HTTP_Client/HTTP_Client.cpp
@@ -17,90 +17,90 @@ using namespace std;
 void filePathManager(char* filePath); // handles the front "/" in file name.
 char* hostname_to_ip(char* hostname); // resolves url to ip address
 void extractContentFromResponse(char* content, char* codeline, int& responseCode);
-bool handleGET (char recvBuff[], char* filePath);
-bool handlePUT (int& client_socket, char* filePath);
+bool handleGET(char recvBuff[], char* filePath);
+bool handlePUT(int& client_socket, char* filePath);
 
 
 int main(int argc, char *argv[]) {
-	
+
 	if (argc != 5) {
 		cout << "Invalid Arguments\nUsage: ./a.out <IP> <PORT> <GET/PUT> <fileName>\nExample: ./a.out 127.0.0.1 80 GET index.html" << endl;
 		return 1;
 	}
-	
+
 	char* hostSupplied = argv[1];
 	char* hostResolved = hostname_to_ip(hostSupplied);
 	int server_port = atoi(argv[2]);
 	char* method = argv[3];
 	char* filePath = argv[4];
-	
+
 	int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-	
+
 	sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(server_port);
+	server_addr.sin_port = htons(server_port);
 	server_addr.sin_addr.s_addr = inet_addr(hostResolved);
 	socklen_t length_server = sizeof(server_addr);
-	
-	connect (client_socket, (sockaddr*) &server_addr, length_server);
-	
+
+	connect(client_socket, (sockaddr*)&server_addr, length_server);
+
 	char sendbuff[BUFF_SIZE];
 	sprintf(sendbuff, "%s %s HTTP/1.0\r\nHost: %s\r\n\r\n", method, filePath, hostSupplied);
-	
-	send (client_socket, sendbuff, BUFF_SIZE, 0);
-	cout << "Client sent "<< sendbuff << endl;
-	
+
+	send(client_socket, sendbuff, BUFF_SIZE, 0);
+	cout << "Client sent " << sendbuff << endl;
+
 	char recvBuff[BUFF_SIZE];
 	recv(client_socket, recvBuff, BUFF_SIZE, 0);
-	
+
 	char responseLine[SMALL_SIZE];
 	int responseCode;
 	extractContentFromResponse(recvBuff, responseLine, responseCode);
 
-	cout << "Client received Response: "<< responseLine << endl;
-	
+	cout << "Client received Response: " << responseLine << endl;
+
 	if (strcmp(method, "GET") == 0 && responseCode == 200) {
-		handleGET (recvBuff, filePath);
+		handleGET(recvBuff, filePath);
 	}
-	else if (strcmp(method, "PUT") == 0 && responseCode == 200 ) {
-		handlePUT (client_socket, filePath);
+	else if (strcmp(method, "PUT") == 0 && responseCode == 200) {
+		handlePUT(client_socket, filePath);
 	}
 
-	cout << "Client Exiting...."<<endl;
+	cout << "Client Exiting...." << endl;
 	close(client_socket);
 
 }
 
 void filePathManager(char* filePath) {
-	
+
 	// if only "/" is supplied, replace it with "index.html"
 	if (strcmp(filePath, "/") == 0) {
 		strcpy(filePath, "index.html");
 	}
-	
+
 	// remove first "/" from the pathname
 	if (filePath[0] == '/') {
 		string s(filePath);
 		const char* temp = s.substr(1).c_str();
 		strcpy(filePath, temp);
 	}
-	
+
 }
 
 char* hostname_to_ip(char* hostname)
 {
-    struct hostent* he = gethostbyname(hostname);
-    struct in_addr** addr_list = (struct in_addr **) he->h_addr_list;
-	  return inet_ntoa(*addr_list[0]);
+	struct hostent* he = gethostbyname(hostname);
+	struct in_addr** addr_list = (struct in_addr **) he->h_addr_list;
+	return inet_ntoa(*addr_list[0]);
 }
 
 void extractContentFromResponse(char* content, char* codeline, int& responseCode) {
-	
+
 	stringstream ss(content);
 	string firstLine;
 	getline(ss, firstLine);
 	strcpy(codeline, firstLine.c_str());
-	
+
 	stringstream ss2(firstLine);
 	ss2 >> firstLine; // waste no use
 	ss2 >> responseCode;
@@ -108,35 +108,35 @@ void extractContentFromResponse(char* content, char* codeline, int& responseCode
 	return;
 }
 
-bool handleGET (char recvBuff[], char* filePath) {
+bool handleGET(char recvBuff[], char* filePath) {
 	cout << "Response Content : \n" << recvBuff << endl;
 	filePathManager(filePath);
-	
+
 	// write to file
 	ofstream client_file(filePath);
 	stringstream ss;
 	ss << recvBuff;
-	
+
 	client_file << ss.str();
 	client_file.close();
-	
-	cout << endl <<"File is saved on the client" << endl;
+
+	cout << endl << "File is saved on the client" << endl;
 }
 
-bool handlePUT (int& client_socket, char* filePath) {
-	
+bool handlePUT(int& client_socket, char* filePath) {
+
 	// read from the file
 	stringstream ss;
 	ifstream client_file(filePath);
 	ss << client_file.rdbuf();
 	string temp = ss.str();
-	
+
 	const char* fileSendBuff = temp.c_str();
-	send (client_socket, fileSendBuff, BUFF_SIZE, 0);
+	send(client_socket, fileSendBuff, BUFF_SIZE, 0);
 	cout << "File sent to the server" << endl;
-	
+
 	client_file.close();
-	
+
 	return true;
-	
+
 }
--- HTTP_Server/HTTP_Server_Concurrent.cpp
+++ HTTP_Server/HTTP_Server_Concurrent.cpp
@@ -16,61 +16,61 @@
 using namespace std;
 
 void filePathManager(char* filePath); // // handles the front "/" in file name.
-bool handleGET (int new_socket, char* recv_buff, char* filePath);
-bool handlePUT (int new_socket, char* recv_buff, char* filePath);
+bool handleGET(int new_socket, char* recv_buff, char* filePath);
+bool handlePUT(int new_socket, char* recv_buff, char* filePath);
 
 int main(int argc, char* argv[]) {
-	
+
 	if (argc != 2) {
 		cout << "Invalid Arguments\nUsage: ./a.out <PORT>\nExample: ./a.out 8521" << endl;
 		return 1;
 	}
-	
+
 	int serv_socket = socket(AF_INET, SOCK_STREAM, 0);
 	int port = atoi(argv[1]);
-	
+
 	sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(port);
+	server_addr.sin_port = htons(port);
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	socklen_t length_server = sizeof(server_addr);
-	
-	bind (serv_socket, (sockaddr*) &server_addr, length_server);
-	
-	listen (serv_socket, QSIZE);
-	
+
+	bind(serv_socket, (sockaddr*)&server_addr, length_server);
+
+	listen(serv_socket, QSIZE);
+
 	while (true) {
-	
+
 		sockaddr_in client_addr;
 		socklen_t clientSize;
-	
-		int new_socket = accept(serv_socket, (sockaddr*) &client_addr, &clientSize);
+
+		int new_socket = accept(serv_socket, (sockaddr*)&client_addr, &clientSize);
 
 		int pid = fork();
 		if (pid == 0) {
-			
+
 			if (new_socket > 0) {
 				cout << "Client connected " << endl;
 			}
-		
+
 			int responseCode;
 			char recv_buff[BUFF_SIZE];
 			char send_buff[BUFF_SIZE];
-			
+
 			recv(new_socket, recv_buff, BUFF_SIZE, 0);
-			
+
 			stringstream ss(recv_buff);
 			string method;
 			ss >> method; // first word
-			
+
 			char filePath[SMALL_SIZE];
 			ss >> filePath; // second word
-			
+
 			if (method.compare("GET") == 0) {
 				handleGET(new_socket, recv_buff, filePath);
 			}
 			else if (method.compare("PUT") == 0) {
-				handlePUT(new_socket, recv_buff, filePath);			
+				handlePUT(new_socket, recv_buff, filePath);
 			}
 			close(new_socket);
 			break; // break the while loop in child 
@@ -78,7 +78,7 @@ int main(int argc, char* argv[]) {
 		else {
 			close(new_socket);
 		}
-	
+
 	}
 	return 0;
 
@@ -86,29 +86,29 @@ int main(int argc, char* argv[]) {
 
 
 void filePathManager(char* filePath) {
-	
+
 	// if only "/" is supplied, replace it with "index.html"
 	if (strcmp(filePath, "/") == 0) {
 		strcpy(filePath, "index.html");
 	}
-	
+
 	// remove first "/" from the pathname
 	if (filePath[0] == '/') {
 		string s(filePath);
 		const char* temp = s.substr(1).c_str();
 		strcpy(filePath, temp);
 	}
-	
+
 }
 
-bool handleGET (int new_socket, char* recv_buff, char* filePath) {
-	
+bool handleGET(int new_socket, char* recv_buff, char* filePath) {
+
 	filePathManager(filePath);
 	cout << "File Path received: " << filePath << endl;
 	stringstream ss;
 	char output[BUFF_SIZE];
 	memset(output, 0, BUFF_SIZE);
-	
+
 	ifstream server_file(filePath);
 	if (!server_file.good()) {
 		sprintf(output, "HTTP/1.0 404 NotFound\r\n\r\n");
@@ -118,40 +118,40 @@ bool handleGET (int new_socket, char* recv_buff, char* filePath) {
 		ss << server_file.rdbuf();
 		string fileContent_string = ss.str();
 		const char* fileContents = fileContent_string.c_str();
-		sprintf(output, "HTTP/1.0 200 OK\r\n\r\n%s", fileContents);	
+		sprintf(output, "HTTP/1.0 200 OK\r\n\r\n%s", fileContents);
 	}
 
-	send (new_socket, output, BUFF_SIZE, 0);
+	send(new_socket, output, BUFF_SIZE, 0);
 	cout << "Data Sent" << endl;
-	
-	server_file.close();	
+
+	server_file.close();
 	return true;
-	
+
 }
 
-bool handlePUT (int new_socket, char* recv_buff, char* filePath) {
-	
+bool handlePUT(int new_socket, char* recv_buff, char* filePath) {
+
 	filePathManager(filePath);
 	cout << "File Path received: " << filePath << endl;
-	
+
 	// first send 200 okay reply
 	char okayReply[SMALL_SIZE];
 	sprintf(okayReply, "HTTP/1.0 200 OK\r\n\r\n");
-	send (new_socket, okayReply, SMALL_SIZE, 0);
-	
+	send(new_socket, okayReply, SMALL_SIZE, 0);
+
 	// wait for the contents to be received
 	recv(new_socket, recv_buff, BUFF_SIZE, 0);
-	
+
 	// write the contents to file
 	ofstream server_file(filePath);
 	stringstream ss;
 	ss << recv_buff;
 	server_file << ss.str();
 	server_file.close();
-	
+
 	cout << "File is created on the server" << endl;
-	send (new_socket, okayReply, SMALL_SIZE, 0);
-	
-	return true;	
-	
+	send(new_socket, okayReply, SMALL_SIZE, 0);
+
+	return true;
+
 }
